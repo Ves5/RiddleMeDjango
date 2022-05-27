d6c5f9d21398b2c250abea3f8a710630c7bcd891
Kacper Ochotny <trinerxd@gmail.com>
2022-05-27 11:25:37 +0200

    before superuser

---
+++ app/riddleme/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RiddlemeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'riddleme'
---
+++ app/riddleme/models.py
@@ -0,0 +1,22 @@
+from pickletools import uint1
+from statistics import mode
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Puzzle(models.Model):
+    title = models.TextField()
+    content = models.TextField()
+    answer = models.CharField(max_length=150)
+    
+class PuzzleStatistics(models.Model):
+    pid = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+    solved_count = models.IntegerField(default=0)
+    first_solve = models.DateTimeField('date of first correct answer')
+    
+class Submitted(models.Model):
+    pid = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+    uid = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    submitted = models.CharField(max_length=150)
+    date = models.DateTimeField('time of answer submission')
+    correct = models.BooleanField()
