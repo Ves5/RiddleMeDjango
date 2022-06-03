27bf9fba06dbcda887c52cbae7946bd25382831c
Kacper Ochotny <trinerxd@gmail.com>
2022-06-03 19:16:16 +0200

    auto stats creationj

--- app/riddleme/models.py
+++ app/riddleme/models.py
@@ -1,20 +1,44 @@
+from datetime import datetime
 from django.db import models
 from django.conf import settings
+from django.db.models import signals
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Puzzle(models.Model):
-    title = models.TextField()
-    content = models.TextField()
-    answer = models.CharField(max_length=150)
+    title = models.TextField(verbose_name="Tytuł")
+    content = models.TextField(verbose_name="Zawartość")
+    answer = models.CharField(max_length=150, verbose_name="Odpowiedź")
+    
+    class Meta:
+        verbose_name = "Zagadka"
+        verbose_name_plural = "Zagadki"
     
 class PuzzleStatistics(models.Model):
-    pid = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
-    solved_count = models.IntegerField(default=0)
-    first_solve = models.DateTimeField('date of first correct answer')
+    pid = models.OneToOneField(Puzzle, on_delete=models.CASCADE, verbose_name="ID zagadki")
+    solved_count = models.IntegerField(default=0, verbose_name="Liczba rozwiązań")
+    first_solve = models.DateTimeField(verbose_name="Data pierwszego rozwiązania")
+    
+    class Meta:
+        verbose_name = "Statystyki zagadek"
+        verbose_name_plural = "Statystyki zagadek"
+    
+# signal method to create puzzle statistic that corresponds to new puzzle
+def create_stats(sender, instance, created, **kwargs):
+    if created:
+        PuzzleStatistics.objects.create(pid = instance, 
+                                        solved_count = 0, 
+                                        first_solve = datetime(year=1970, month=1, day=1))
+signals.post_save.connect(create_stats, sender=Puzzle, weak=False, dispatch_uid="riddleme.models.create_stats")
     
 class Submitted(models.Model):
-    pid = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
-    uid = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    submitted = models.CharField(max_length=150)
-    date = models.DateTimeField('time of answer submission')
-    correct = models.BooleanField()
+    pid = models.ForeignKey(Puzzle, on_delete=models.CASCADE, verbose_name="ID zagadki")
+    uid = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="ID użytkownika")
+    submitted = models.CharField(max_length=150, verbose_name="Zgłoszona odpowiedź")
+    date = models.DateTimeField(verbose_name="Data zgłoszenia odpowiedzi")
+    correct = models.BooleanField(verbose_name="Czy poprawna odpowiedź")
+    
+    class Meta:
+        verbose_name = "Zgłoszenie"
+        verbose_name_plural = "Zgłoszenia"
